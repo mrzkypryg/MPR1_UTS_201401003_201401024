e079244ff012476355a5ef6882cd0be561128ec2
Muhammad Rizky Prayoga_024 <mrpharyga2019@gmail.com>
2022-11-15 22:59:41 +0700

    Mengubah warna background button di ProfileFragment dan menambahkan Toast pada masing-masing button

--- app/src/main/java/com/example/mpr1_uts_201401003_201401024/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/mpr1_uts_201401003_201401024/ui/profile/ProfileFragment.kt
@@ -1,10 +1,15 @@
 package com.example.mpr1_uts_201401003_201401024.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.example.mpr1_uts_201401003_201401024.MainActivity
 import com.example.mpr1_uts_201401003_201401024.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,6 +26,9 @@ class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var btnAS : Button
+    lateinit var btnMC : Button
+    lateinit var btnSub : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +44,25 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnAS = view.findViewById(R.id.btnAccSet)
+        btnAS.setOnClickListener(){
+            Toast.makeText(activity, "Not Available yet", Toast.LENGTH_SHORT).show()
+        }
+        btnMC = view.findViewById(R.id.btnMyCr)
+        btnMC.setOnClickListener(){
+            Toast.makeText(activity, "Not Available yet", Toast.LENGTH_SHORT).show()
+        }
+        btnSub = view.findViewById(R.id.btnSubs)
+        btnSub.setOnClickListener(){
+            Toast.makeText(activity, "Not Available yet", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
@@ -56,5 +83,6 @@ class ProfileFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+
     }
 }
